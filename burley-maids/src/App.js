fffa1ea56a8c79ef9f91c15264c0521f6f6b7727
John-Peter Eberhard <johnpetereberhard@gmail.com>
2023-05-30 15:04:51 -0600

    Initial website build

---
+++ burley-maids/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         THIS WEBSITE IS BEING MAID FOR JOE LADAN. 
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         A website by john 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
