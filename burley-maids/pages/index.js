88fb48ad738f5df3a779962931750b1bc6792012
John-Peter Eberhard <johnpetereberhard@gmail.com>
2023-06-05 05:02:09 -0600

    testing next.js

---
+++ burley-maids/pages/index.js
@@ -0,0 +1,11 @@
+[label /pages/index.js]
+
+import React, { Component } from 'react'
+
+export default class extends Component {
+  render () {
+    return (
+      <div>Your Next.js App</div>
+    )
+  }
+}
