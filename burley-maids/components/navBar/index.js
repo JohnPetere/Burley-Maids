fc394ee064e8713e4d55933c1a19fb5544a42c6f
John-Peter Eberhard <johnpetereberhard@gmail.com>
2023-06-25 00:24:31 -0600

    test thing

---
+++ burley-maids/components/navBar/index.js
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+import './styles.css';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <img className="logo" src="/logo.svg" alt="Logo" />
+            </div>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <a
+                href="/"
+                className="link"
+              >
+                Home
+              </a>
+              <a
+                href="/about"
+                className="link"
+              >
+                About
+              </a>
+              <a
+                href="/services"
+                className="link"
+              >
+                Services
+              </a>
+              <a
+                href="/contact"
+                className="link"
+              >
+                Contact
+              </a>
+            </div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={toggleNavbar}
+              type="button"
+              className="menu-toggle"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open menu</span>
+              {!isOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* open mobile menu */}
+      {isOpen && (
+        
+        <div className="md:hidden mobile-menu" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <a
+              href="/"
+              className="link"
+            >
+              Home
+            </a>
+            <a
+              href="/about"
+              className="link"
+            >
+              About
+            </a>
+            <a
+              href="/services"
+              className="link"
+            >
+              Services
+            </a>
+            <a
+              href="/contact"
+              className="link"
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
--- burley-maids/pages/index.js
+++ burley-maids/pages/index.js
@@ -1,10 +1,10 @@
 
-
+import NavBar from "../components/navBar"
 export default function Home() {
   return (
    
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div>
+    <NavBar/>
+  </div>
   )
 }
