2e11da8adfb627ef4a1784bb8df820e3182d170d
John-Peter Eberhard <johnpetereberhard@gmail.com>
2023-08-11 19:40:37 -0600

    clean up and set up for the carasoul

--- burley-maids/components/carouselMainPage/index.js
+++ burley-maids/components/carouselMainPage/index.js
@@ -1,9 +1,11 @@
 
 import Image from 'next/image';
 import { useState } from 'react';
-
+import slideData from './slideData';
+// I am probably going to use getServerSideProps....
+// then use Map??? well see....
 let CarasuselMainPage = () => {
-  let goToSLilde = ()=>{
+  let goToSlide = ()=>{
 
   }
   let nextSlide =()=>{
@@ -12,6 +14,10 @@ let CarasuselMainPage = () => {
   let prevSlide = ()=>{
 
   }
+  let populateSlides = ()=>{
+
+  }
+
 
   return (
     <div className=" h-96 w-full  
@@ -75,30 +81,6 @@ let CarasuselMainPage = () => {
       {/* Button nav things here */}
       <div>
       
-      {/* <div
-    class="absolute bottom-0 left-0 right-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
-    data-te-carousel-indicators>
-    <button
-      type="button"
-      data-te-target="#carouselExampleCaptions"
-      data-te-slide-to="0"
-      data-te-carousel-active
-      class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-      aria-current="true"
-      aria-label="Slide 1"></button>
-    <button
-      type="button"
-      data-te-target="#carouselExampleCaptions"
-      data-te-slide-to="1"
-      class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-      aria-label="Slide 2"></button>
-    <button
-      type="button"
-      data-te-target="#carouselExampleCaptions"
-      data-te-slide-to="2"
-      class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-      aria-label="Slide 3"></button>
-  </div> */}
       </div>
     </div>
   );
---
+++ burley-maids/components/carouselMainPage/slideData.js
@@ -0,0 +1,28 @@
+let slideData = [
+    {
+      imageURL:"",
+      header1:"",
+      header2:"",
+      buttonText:"",
+      buttonLink:"",
+
+    },
+    {
+      imageURL:"",
+      header1:"",
+      header2:"",
+      buttonText:"",
+      buttonLink:"",
+
+    },
+    {
+      imageURL:"",
+      header1:"",
+      header2:"",
+      buttonText:"",
+      buttonLink:"",
+
+    },
+  ]
+
+  export default slideData;
