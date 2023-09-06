2e94abcd603d519f954d634367c8957542311b6e
John-Peter Eberhard <johnpetereberhard@gmail.com>
2023-09-06 02:08:04 -0600

    testing font usage

--- burley-maids/pages/_app.js
+++ burley-maids/pages/_app.js
@@ -1,11 +1,11 @@
 import Navbar from '../components/navBar'
 import '../styles/globals.css'
 
-// import localFont from 'next/font/local'
-// const BrushScript = localFont({src: './fonts/brushScript/Brush Script Regular.ttf'})
+ import localFont from 'next/font/local'
+const BrushScript = localFont({src: './fonts/brushScript/Brush Script Regular.ttf'})
 export default function MyApp({ Component, pageProps }) {
   
-  return <Component className=" h-auto w-screen flex flex-col " {...pageProps}>
+  return <Component className={`${BrushScript.className} h-auto w-screen flex flex-col `} {...pageProps}>
 
    <Navbar/>
   </Component>
