d8b72bd4e0fa76fd27e3b7e2864fff42c84f4deb
John-Peter Eberhard <johnpetereberhard@gmail.com>
2023-10-09 02:57:05 -0600

    more testimonies shit

--- burley-maids/pages/Testimonies/index.js
+++ burley-maids/pages/Testimonies/index.js
@@ -10,23 +10,23 @@ function Testimonies({ children, ...props }) {
     "
     >
       {children}
-      <h2 className="m-5">Testimonies</h2>
-      <div>
+      <div className="w-full h-full ">
+      <h2 className="m-5  text-center">Testimonies</h2>
+      <div
+      className="flex flex-wrap  w-full" 
+      >
+
         {testimonyData.map((testimoney) => (
-          <div className="text-base font-normal">
-            <p
-           
-            >{testimoney.desc}</p>
-            <p
-            className="text-zaffra font-bigBold"
-            >{testimoney.author}</p>
-            <p
-            className="font-bold"
-            >
-              {testimoney.location}
-            </p>
-          </div>
+          
+            <div className="text-base  font-normal h-1/4 w-80 m-7 p-6 bg-lavendarWeb flex flex-col">
+              <p>{testimoney.desc}</p>
+              <p  className="text-zaffra font-bigBold text-right px-6
+               justify-self-center">{testimoney.author}</p>
+              <p className=" text-right px-6">{testimoney.location}</p>
+            </div>
+        
         ))}
+      </div>
       </div>
     </div>
   );
