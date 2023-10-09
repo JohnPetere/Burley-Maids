d5d5a2c767afbf13ae47667d66dbb9a3cbf5446b
John-Peter Eberhard <johnpetereberhard@gmail.com>
2023-10-09 14:36:19 -0600

    Added more slides

--- burley-maids/pages/Testimonies/testimoneyData.js
+++ burley-maids/pages/Testimonies/testimoneyData.js
@@ -20,9 +20,21 @@ var testimonyData = [
   
       },
       {
+        desc:"Man, let me tell you about this cleaning company. I can't get enough of 'em! Their service is so damn reliable and thorough that it leaves my home looking and smelling absolutely amazing. When they're done. It's a damn shame they don't offer their services to the Avengers, 'cause they could sure use it to keep the Tower clean!" ,
+        author: "Not Sam Jack",
+        location: "Ice District"
+  
+      },
+      {
         desc:"I have tried many cleaning services in the past, but none of them can match the level of service and quality provided by this company. They always exceed expectations and ensure every nook and cranny of my home is spotless. Their attention to detail is impressive, most impressive. I cannot recommend them enough. The power of cleanliness is strong with this company. Join them, and your home will be transformed." ,
         author: "Not Darth Vader",
         location: "Not Texas"
+  
+      },
+      {
+        desc:"For several months, this cleaning company I have used. Always impressed with their service, I am. Reliable, thorough, and always leave my home looking and smelling great, they do. Love coming home after they have been there, I do. Like a weight lifted off my shoulders, it feels. Highly recommend them to anyone looking for a quality cleaning service, I do. Trust me, disappointed you will not be. Give them a chance, you must!" ,
+        author: "Not Yoda",
+        location: "Millwoods"
   
       },
   ]
